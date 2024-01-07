2daa6b45ee12bcdb0123caea52e9920b22d3318c
pbkswift <prarthanadhanya2010@gmail.com>
2024-01-07 18:09:17 +0530

    interactive-prediction

---
+++ interactive-predicition.py
@@ -0,0 +1,13 @@
+# Interactive-predicition.py
+
+
+symptom1 = input ("enter symptom1 ;")
+symptom2 = input ("enter symptom2 ;")
+symptom3 = input ("enter symptom3 ;")
+
+
+combinedsymptoms= symptom1+","+symptom2+","+symptom3
+
+print(combinedsymptoms)
+
+
